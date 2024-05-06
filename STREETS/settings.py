da91bab47f630255e07ea413231f9d119566889f
amalsiva7 <amalsiva7210@gmail.com>
2024-05-06 18:59:35 +0530

    staticfiles path changed

--- STREETS/settings.py
+++ STREETS/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'STREETS/static')
 ]
 
 STATIC_ROOT = BASE_DIR / "static"

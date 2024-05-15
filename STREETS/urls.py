8a75bd54a04e8088e8120e1238e83c2024c64bce
amalsiva7 <amalsiva7210@gmail.com>
2024-05-15 17:46:26 +0530

    edit in settings url pattern media file

--- STREETS/urls.py
+++ STREETS/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
-# from . import views
 from django.conf import settings
 
 urlpatterns = [
@@ -31,6 +30,7 @@ urlpatterns = [
     path('payment/', include('payment.urls')),
     path('', include('wishlist.urls')),
     path('', include('wallet.urls')),
-    
-    
- ] + static(settings.MEDIA_URL,document_root= settings.MEDIA_ROOT)
+ ]
+
+if settings.DEBUG is True:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
